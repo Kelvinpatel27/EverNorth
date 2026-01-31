7bf44a3ebddf85ca24f87554624f01f77fa57194
Kelvinpatel27 <kelvinpatel27122004@gmail.com>
2026-01-31 19:12:37 +0530

    fix

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,7 @@ import google.generativeai as genai
 
 load_dotenv()
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+print("GEMINI_API_KEY:", GEMINI_API_KEY)
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel('gemini-2.5-flash')
 
